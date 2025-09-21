39fc0b80f1675db0f4d33d2899dd90491d6a09f7
wuyugu <16088336+wuyugu16@user.noreply.gitee.com>
2025-09-21 08:39:11 +0800

    1.0.1 better index

--- data.js
+++ data.js
@@ -15,29 +15,60 @@ var classname = [
     "文章",
     "工具",
     "系统"
-]
+];
 var pages = [
     {
-        title:"所有页面",
+        title:"首页",
         class:6,
         content:()=>{
             var res = "";
-            pages.forEach((item,index)=>{
-                res += `<div id="P0-wrap">
-                <h2><a href="#" onclick="update(${index})">
-                <span style="color:${classcolor[item.class]}">[${classname[item.class]}]<span>
-                ${item.title}
-                </h2></a>
-                </div>`;
-            })
+            const get = (sclass,sname)=>{
+                res += `<fieldset class="P0-fieldset"><legend>${sname}</legend>`;
+                Array
+                    .from({length: pages.length}, (_, i) => i)
+                    .filter(item=>pages[item].class==sclass)
+                    .sort((a,b)=>Math.random()-0.5)
+                    .slice(0,5)
+                    .forEach(item=>{
+                        res += `<a onclick="update(${item})">${pages[item].title}</a><br/>`
+                    });
+                res += "</fieldset>";
+            };
+            get(0,"梗");
+            get(4,"文章");
+            //get(5,"工具");
             return res;
+        }
+    },
+
+    {
+        title:"wiki中心页",
+        class:6,
+        content:()=>{
+            var res = "<fieldset><legend>梗</legend>";
+            Array
+                .from({length:pages.length},(_,i)=>i)
+                .filter(item=>pages[item].class==0)
+                .sort((a,b)=>pages[a].title>pages[b].title)
+                .forEach(item=>{
+                    res += `<a onclick="update(${item})">${pages[item].title}</a>`;
+                });
+            res += "</fieldset>";
+            return res;
+        }
+    },
+    {
+        title:"再来一遍",
+        class:0,
+        content:()=>{
+
         }
     },
     {
         title:"南湖史",
         class:4,
         content:()=>{
-
+            return "测测测测";
         }
     }
 ];
