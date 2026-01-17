269eed20bc44acc00a0b514c5c972c6b55fdec1c
wuyugu <wuyugu3624175@outlook.com>
2026-01-17 09:49:15 +0800

    add kind-1

--- src/list.js
+++ src/list.js
@@ -2,8 +2,8 @@ var pagesList = {
     object : {type: "html", label: [], kind: "system", title: "Obejct"},
     test : {type: "text", label: [], kind: "system", title:"111"},
     test2 : {type: "html", label: ["test0"], kind: "tool", title:"111"},
-    test3 : {type: "markdown", label: ["test0","test1"], kind: "passages", title:"111"},
-    test4 : {type: "text", label: ["test1"], kind: "game", title:"111"}
+    test3 : {type: "markdown", label: ["test0","test1"], kind: "article", title:"111"},
+    test4 : {type: "text", label: ["test1"], kind: "fun", title:"111"}
 };
 var labels = {};
 Object.keys(pagesList).forEach(item=>{
@@ -30,4 +30,14 @@ function get(p,info){
     }
     var filename = `./pages/${p}.${{html:"html",markdown:"md",text:"txt"}[info.type]}`;
     return fetch(filename).then(data=>data.text());
+}
+function getTitle(p){
+    var c = {
+        system : ["#aaa","系统"],
+        tool   : ["#4dd","工具"],
+        fun    : ["#d4d","有趣"],
+        article: ["#e55","文章"]
+    }
+    var info = pagesList[p];
+    return `<b><span style="color:${c[info.kind][0]}">[${c[info.kind][1]}]</span> ${info.title}</b>`;
 }
